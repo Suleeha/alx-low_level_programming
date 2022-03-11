02f4f101304800e059b804d368fe89b092eee8f5
suleeha <suleeha@gmail.com>
2022-03-11 12:26:11 -0800

    9printcomb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers with commas using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int x = 0;
+while (x < 10)
+{
+putchar(x + '0');
+if (x == 9)
+{
+putchar('\n');
+break;
+}
+putchar(',');
+putchar(' ');
+x++;
+}
+return (0);
+}
