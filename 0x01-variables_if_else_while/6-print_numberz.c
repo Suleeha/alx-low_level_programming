cd87fbb36e8d6eb85e3fa187d2593420e0030528
suleeha <suleeha@gmail.com>
2022-03-11 12:11:52 -0800

    6printnumberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers 0-9 with putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int x = 0;
+while (x < 10)
+{
+putchar(x + '0');
+x++;
+}
+putchar('\n');
+return (0);
+}
