cbe91c41a3968c35a7119d89bce0b0e5b2ea6e69
suleeha <suleeha@gmail.com>
2022-03-17 12:21:59 -0700

    printnumber

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - print an integer using _putchar
+ * @n: character to check
+ *
+ * Return: void
+ *
+ */
+void print_number(int n)
+{
+unsigned int abs;
+if (n < 0)
+{
+_putchar('-');
+abs = -n;
+}
+else
+abs = n;
+if (abs / 10)
+print_number(abs / 10);
+_putchar(abs % 10 + '0');
+}
